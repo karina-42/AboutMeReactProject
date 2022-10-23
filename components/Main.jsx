297a22add2c7490e2593c4913b0ff3928de671d8
Angela Vegega <angela.vegega@gmail.com>
2022-10-23 21:21:25 +0900

    first commit

---
+++ components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Main() {
+    return (
+      <main className="main">
+        <h1 className="main-title">Fun facts about me</h1>
+        <ul className="main-list">
+            <li className="main-list-item">I'm learning how to play golf now</li>
+            <li className="main-list-item">I do weight training 5 days a week</li>
+            <li className="main-list-item">I can speak Spanish, English, and Japanese</li>
+            <li className="main-list-item">I broke my neck in a car accident</li>
+            <li className="main-list-item">I love food and can eat anything</li>                
+        </ul>
+      </main>
+    )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            {/* <img src="../images/react-icon-small.png" className="img"/> */}
+            <h2 className="nav-logo-text">KarinaFacts</h2>
+            <h3 className="nav-title">Get to know me</h3>
+        </nav>    
+    )
+}
