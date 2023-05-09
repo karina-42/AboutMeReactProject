8047b3f90c269fc9f0582f0a8b13d0a322feeb7e
Angela Vegega <angela.vegega@gmail.com>
2023-05-09 12:50:00 +0900

    Add image of myself to Main

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <img src="../images/logo.png" className="img"/>
+            <img src="../images/logo.png" className="logo-img"/>
             <h2 className="nav-logo-text">Facts</h2>
             <h3 className="nav-title">Get to know me</h3>
         </nav>    
