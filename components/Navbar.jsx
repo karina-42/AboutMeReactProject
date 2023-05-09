25332c43961891d35c090d8a2ef334a102366d45
Angela Vegega <angela.vegega@gmail.com>
2023-05-09 12:30:57 +0900

    Add logo picture

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,8 +3,8 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav className="navbar">
-            {/* <img src="../images/react-icon-small.png" className="img"/> */}
-            <h2 className="nav-logo-text">KarinaFacts</h2>
+            <img src="../images/logo.png" className="img"/>
+            <h2 className="nav-logo-text">Facts</h2>
             <h3 className="nav-title">Get to know me</h3>
         </nav>    
     )
